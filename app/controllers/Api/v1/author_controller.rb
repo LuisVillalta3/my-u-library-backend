0823019243da25d77b2d513b06bc94a7fa6685b7
luisvillalta7 <luisvillalta3@gmail.com>
2022-09-14 20:03:45 -0600

    Author initial crud

--- app/controllers/Api/v1/author_controller.rb
+++ app/controllers/Api/v1/author_controller.rb
@@ -2,4 +2,32 @@ class Api::V1::AuthorController < ApplicationController
   def index
     render json: Author.all
   end
+
+  def show
+    render json: Author.find(params[:id])
+  end
+
+  def create
+    author = Author.new(author_params)
+
+    return render json: author.errors, status: :unprocessable_entity if !author.save
+    render json: author, status: :created
+  end
+
+  def update
+    author = Author.find(params[:id])
+    return render json: author.errors, status: :unprocessable_entity if !author.update(author_params)
+    render json: author, status: :ok
+  end
+
+  def destroy
+    author = Author.find(params[:id])
+    return render json: author.errors, status: :unprocessable_entity if !author.destroy
+    render json: author, status: :ok
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:firstName, :lastName, :nacionality, :birthday)
+  end
 end
