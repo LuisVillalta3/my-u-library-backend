f273637ab0a2c479c47eca5c647d6f9d3272de6c
Luis Villalta <luis.villaltar@salud.gob.sv>
2022-09-20 07:49:46 -0600

    Devise

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,5 +1,5 @@
 class JsonWebToken
-  SECRET_KEY = Rails.application.credentials.devise[:jwt_secret_key].to_s
+  SECRET_KEY = 'b46f9fd955a29b47ed9c8554b10ea7672c6872360b222ceead97a098b1049fba199cf187533c2979eda378407a96f4d0a15a1812f4dcf3aad2272c56081e7d8f'
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
